ac4223c4c2ba817d056e67749dcf1b08ad09684f
alkadoHs <alkadosichone@gmail.com>
2024-10-10 06:10:53 +0300

    st observer

--- app/Models/StockTransfer.php
+++ app/Models/StockTransfer.php
@@ -2,10 +2,13 @@
 
 namespace App\Models;
 
+use App\Observers\StockTransferObserver;
+use Illuminate\Database\Eloquent\Attributes\ObservedBy;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+#[ObservedBy(StockTransferObserver::class)]
 class StockTransfer extends Model
 {
     use HasFactory;
