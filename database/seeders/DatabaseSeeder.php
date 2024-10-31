7857634a451b1c83c60451bcdaf852f284a2d0dc
alkadoHs <alkadosichone@gmail.com>
2024-10-31 15:49:35 +0300

    ...

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,13 @@
 
 namespace Database\Seeders;
 
+use App\Models\Branch;
+use App\Models\Company;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,9 +19,30 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
+        Company::factory()->create([
+            'name' => 'Mauzodata Company LTD',
+            'address' => 'P.O.BOX 295, Ilomba, Mbeya',
+            'email' => 'mauzodata@gmail.com',
+            'phone' => '0760000000',
+        ]);
+
+        Branch::factory()->create([
+            'company_id' => 1,
+            'name' => 'MAUZODATA SHOP',
+            'address' => 'P.O.BOX 295, Ilomba, Mbeya',
+            'phone' => '076000000000',
+        ]);
+
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'branch_id' => 1,
+            'company_id' => 1,
+            'name' => 'Alkado Hs',
+            'email' => 'alkado@gmail.com',
+            'role' => 'admin',
+            'phone' => '07600000000',
+            'email_verified_at' => now(),
+            'password' => Hash::make('Alkado$255'),
+            'remember_token' => Str::random(10)
         ]);
     }
 }
